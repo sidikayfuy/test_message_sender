57527921e44284b888aaab61c63da3c558d0f403
Max <cherepp.01@gmail.com>
2022-09-27 18:00:26 +0300

    cors

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Test work message sender
+
+Для запуска необходимо загрузить архив с кодом и извлечь. 
+
+Через терминал перейти в папку с извлеченными файлами и запустить команду:
+
+````
+docker-compose up --build -d && docker exec -it test_message_sender-main-webapp-1 bash -c "celery -A test_message worker --loglevel=info" 
+````
+
--- test_message/settings.py
+++ test_message/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'messageapi',
 
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,22 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://editor.swagger.io",
+    "http://localhost:8000",
+]
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
 ]
 
 ROOT_URLCONF = 'test_message.urls'
